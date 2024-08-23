8783a4108194512e46dc98b81ee9916bca2d5dca
ayodiya <devayodiya@gmail.com>
2024-08-23 06:44:52 +0100

    add   validator/addcontactvalidator  addcontactvalidator

---
+++ app/validators/addContactValidator.tsx
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+export const FIRST_NAME = "firstName";
+export const FIRST_NAME_LABEL = "First Name";
+
+export const LAST_NAME = "lastName";
+export const LAST_NAME_LABEL = "Last Name";
+
+export const PHONE_NUMBER = "phoneNo";
+export const PHONE_NUMBER_LABEL = "Phone Number";
+
+export const EMAIL = "email";
+export const EMAIL_LABEL = "Email";
+
+export default Yup.object({
+  [FIRST_NAME]: Yup.string().label(FIRST_NAME_LABEL).required(),
+  [LAST_NAME]: Yup.string().label(LAST_NAME_LABEL).required(),
+  [PHONE_NUMBER]: Yup.string().label(PHONE_NUMBER_LABEL).required(),
+  [EMAIL]: Yup.string().label(EMAIL_LABEL).email("Invalid email").required(),
+});
